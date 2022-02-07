aea66f112b1af496dd02f57914a085617913d3d2
Vadym <vadym.snigur.code@gmail.com>
2022-02-07 22:52:08 +0200

    first commit

---
+++ src/main/java/com/example/springtransactionjpa/entity/BankAccount.java
@@ -0,0 +1,43 @@
+package com.example.springtransactionjpa.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Bank_Account" )
+public class BankAccount {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "FULL_NAME", length = 128, nullable = false)
+    private String fullName;
+
+    @Column(name = "BALANCE", nullable = false)
+    private double balance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
